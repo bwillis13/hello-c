6a0cb801907350313f53f640ea711aaf3c093cab
Brian Willis <bwillis@cloudbees.com>
2018-05-03 13:31:37 -0700

    world - brian
    change world - brian

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   if (argc > 1) {
     printf("Hello %s!\n", argv[1]) ;
   } else {
-    printf("Hello world!\n");
+    printf("Hello Brian\n");
   }
   return 0;
 }
